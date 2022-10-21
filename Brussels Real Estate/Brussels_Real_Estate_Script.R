4ca67c21a088df23471e9c867251f3c496744eaf
Daniel Verdon <danielverdon@gmail.com>
2022-10-21 22:37:31 +0200

    Cleaning my coding.

--- Brussels Real Estate/Brussels_Real_Estate_Script.R
+++ Brussels Real Estate/Brussels_Real_Estate_Script.R
@@ -7,6 +7,7 @@ library(ggthemes) #Nice theme for maps
 library(ggrepel)
 library(lintr)
 library(dbplyr)
+library(scales)
 
 ###MAP CODING
 #Import Shp file and plot
@@ -18,8 +19,7 @@ emptymap <- emptymap %>%
 
 ###DATA CODING
 #Importing Municipality Date and first filter
-housing_brussels <- "Brussels Real Estate/export.csv"
-dat_original <- read.csv(housing_brussels)
+dat_original <- read.csv("Brussels Real Estate/export.csv")
 
 #ORIGINAL DAT & rename columns and values
 dat <-
@@ -38,33 +38,22 @@ dat <-
 
 #Add shortened Names
 dat$Commune_short <- as.character(dat$Commune_short)
-dat$Commune_short[dat$Commune_short == "Berchem-Sainte-Agathe"] <-
-  "Berchem"
-dat$Commune_short[dat$Commune_short == "Forest (Bruxelles-Capitale)"] <-
-  "Forest"
-dat$Commune_short[dat$Commune_short == "Molenbeek-Saint-Jean"] <-
-  "Molenbeek"
-dat$Commune_short[dat$Commune_short == "Saint-Josse-ten-Noode"] <-
-  "St-Josse"
-dat$Commune_short[dat$Commune_short == "Watermael-Boitsfort"] <-
-  "Boitsfort"
+dat$Commune_short[dat$Commune_short == "Berchem-Sainte-Agathe"] <- "Berchem"
+dat$Commune_short[dat$Commune_short == "Forest (Bruxelles-Capitale)"] <- "Forest"
+dat$Commune_short[dat$Commune_short == "Molenbeek-Saint-Jean"] <- "Molenbeek"
+dat$Commune_short[dat$Commune_short == "Saint-Josse-ten-Noode"] <- "St-Josse"
+dat$Commune_short[dat$Commune_short == "Watermael-Boitsfort"] <- "Boitsfort"
 dat$Commune_short[dat$Commune_short == "Bruxelles"] <- "Bxl"
-dat$Commune_short[dat$Commune_short == "Woluwe-Saint-Pierre"] <-
-  "WSP"
-dat$Commune_short[dat$Commune_short == "Woluwe-Saint-Lambert"] <-
-  "WSL"
+dat$Commune_short[dat$Commune_short == "Woluwe-Saint-Pierre"] <- "WSP"
+dat$Commune_short[dat$Commune_short == "Woluwe-Saint-Lambert"] <- "WSL"
 dat$Commune_short <- as.factor(dat$Commune_short)
 
 
 ##CLEANING
 #Translate Values
 dat$Building.type <- as.character(dat$Building.type)
-dat$Building.type[dat$Building.type ==
-  "Maisons avec 2 ou 3 façades (type fermé + type demi-fermé)"] <-
-  "Terraced or Semi-terraced"
-dat$Building.type[dat$Building.type ==
-  "Appartements"] <-
-  "Apartments"
+dat$Building.type[dat$Building.type == "Maisons avec 2 ou 3 façades (type fermé + type demi-fermé)"] <- "Terraced or Semi-terraced"
+dat$Building.type[dat$Building.type == "Appartements"] <- "Apartments"
 dat$Building.type <- as.factor(dat$Building.type)
 
 #Filter out uneeded values
@@ -89,21 +78,30 @@ dat_allyears_appart_nonas <-
   dat %>% filter(Building.type == "Apartments" & !is.na(Median.price))
 
 ###DF % Median Price increase Apartments
-dat_pcincrease_appart <- dat %>% select(Commune,Commune_short,Year,Building.type,Sqm,Median.price) %>%
+dat_pcincrease_appart <- dat %>% 
+  select(Commune,Commune_short,Year,Building.type,Sqm,Median.price) %>%
   filter(Building.type=="Apartments" & Sqm=="Superficie inconnue") %>% 
   pivot_wider(names_from = Year,values_from = Median.price)
+
 colnames(dat_pcincrease_appart)[5:17] <- paste("year_",colnames(dat_pcincrease_appart)[5:17],"")
-dat_pcincrease_appart <- dat_pcincrease_appart %>% mutate('increase2010_2022'=as.integer((`year_ 2022 `/`year_ 2010 `-1)*100),'increase2015_2022'=as.integer((`year_ 2022 `/`year_ 2017 `-1)*100),,'increase2020_2022'=as.integer((`year_ 2022 `/`year_ 2020 `-1)*100))
+dat_pcincrease_appart <- dat_pcincrease_appart %>% 
+  mutate('increase2010_2022'=as.integer(((`year_ 2022 `-`year_ 2010 `)/`year_ 2022 `)*100),
+         'increase2015_2022'=as.integer(((`year_ 2022 `-`year_ 2015 `)/`year_ 2022 `)*100),
+         'increase2020_2022'=as.integer(((`year_ 2022 `-`year_ 2020 `)/`year_ 2022 `)*100)
+         )
 
 
 
 ###MERGE DATA AND MAP CODING
 dat_2022_appart_nonas_merged <-
-  left_join(emptymap, dat_2022_appart_nonas, by = "Commune") %>%  filter(!is.na(Year))
+  left_join(emptymap, dat_2022_appart_nonas, by = "Commune") %>%
+  filter(!is.na(Year))
 dat_allyears_appart_nonas_merged <-
-  left_join(emptymap, dat_2022_appart_nonas, by = "Commune") %>%  filter(!is.na(Year))
+  left_join(emptymap, dat_2022_appart_nonas, by = "Commune") %>%
+  filter(!is.na(Year))
 dat_pcincrease_appart_merged <-
-  left_join(emptymap, dat_pcincrease_appart, by = "Commune") %>% filter(!is.na(increase2010_2022))
+  left_join(emptymap, dat_pcincrease_appart, by = "Commune") %>%
+  filter(!is.na(increase2010_2022))
 
 
 #Create the Map/Plot
@@ -177,6 +175,13 @@ ggplot(dat_pcincrease_appart_merged) +
         legend.key.size = unit(5, "mm"))
 
 ###LINE Dat_all communes
-dat_allyears_appart_nonas %>% filter(Commune_short %in% c("St-Josse","Ixelles","WSP","Forest","Saint-Gilles","Uccle")) %>% 
-ggplot(aes(x=as.factor(Year),y=Median.price,group=Commune_short,color=Commune_short)) + 
-  geom_line() +theme_base()
+dat_allyears_appart_nonas %>% 
+  filter(Commune_short %in% c("St-Josse","Ixelles","WSP","Forest","Saint-Gilles","Uccle")) %>%
+  ggplot(aes(x=as.factor(Year),y=Median.price,group=Commune_short,color=Commune_short)) +
+  geom_line() +
+  theme_base() +
+  scale_y_continuous(name="Average Price", labels=dollar_format(suffix="€",prefix="")) +
+  scale_x_discrete(name="Year")+
+  ggthemes::theme_economist()
+  
+  
